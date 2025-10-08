8d5f862f10e8fc6e67bfd95388b2c958a857eb00
rajmanna <dev.rajm8923@gmail.com>
2025-10-08 11:21:48 +0530

    added using namespace std

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,45 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
-bool match_pattern(const std::string& input_line, const std::string& pattern) {
-    if (pattern.length() == 1) {
-        return input_line.find(pattern) != std::string::npos;
-    }
-    else {
-        throw std::runtime_error("Unhandled pattern " + pattern);
-    }
+bool match_pattern(const string &input_line, const string &pattern) {
+  if (pattern.length() == 1) {
+    return input_line.find(pattern) != string::npos;
+  } else {
+    throw runtime_error("Unhandled pattern " + pattern);
+  }
 }
 
-int main(int argc, char* argv[]) {
-    // Flush after every std::cout / std::cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
+int main(int argc, char *argv[]) {
+  cout << unitbuf;
+  cerr << unitbuf;
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here" << std::endl;
+  cerr << "Logs from your program will appear here" << endl;
 
-    if (argc != 3) {
-        std::cerr << "Expected two arguments" << std::endl;
-        return 1;
-    }
+  if (argc != 3) {
+    cerr << "Expected two arguments" << endl;
+    return 1;
+  }
 
-    std::string flag = argv[1];
-    std::string pattern = argv[2];
+  string flag = argv[1];
+  string pattern = argv[2];
 
-    if (flag != "-E") {
-        std::cerr << "Expected first argument to be '-E'" << std::endl;
-        return 1;
-    }
-
-    // Uncomment this block to pass the first stage
-    //
-    // std::string input_line;
-    // std::getline(std::cin, input_line);
-    //
-    // try {
-    //     if (match_pattern(input_line, pattern)) {
-    //         return 0;
-    //     } else {
-    //         return 1;
-    //     }
-    // } catch (const std::runtime_error& e) {
-    //     std::cerr << e.what() << std::endl;
-    //     return 1;
-    // }
+  if (flag != "-E") {
+    cerr << "Expected first argument to be '-E'" << endl;
+    return 1;
+  }
+ 
+   string input_line;
+   getline(cin, input_line);
+  
+   try {
+       if (match_pattern(input_line, pattern)) {
+           return 0;
+       } else {
+           return 1;
+       }
+   } catch (const runtime_error& e) {
+       cerr << e.what() << endl;
+       return 1;
+   }
 }
